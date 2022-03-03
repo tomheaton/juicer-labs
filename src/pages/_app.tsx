c23acaca2821a1e9129047fcad245ee45bd4ac9e
Tom Heaton <thomas.m.heaton@gmail.com>
2022-03-03 23:16:03 +0000

    cleanup :sponge:

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css';
+import {AppProps} from 'next/app';
+
+const App = ({ Component, pageProps }: AppProps) => {
+
+    return (
+        <Component {...pageProps} />
+    );
+}
+
+export default App;
