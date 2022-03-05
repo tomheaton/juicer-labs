380531ad25b132ed00afce090627d51d131618ca
Tom Heaton <thomas.m.heaton@gmail.com>
2022-03-05 22:41:03 +0000

    add link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import {NextPage} from 'next';
 import Head from 'next/head';
 import styles from '../styles/Index.module.css';
+import Link from "next/link";
 
 const Index: NextPage = () => {
 
@@ -16,6 +17,17 @@ const Index: NextPage = () => {
                 <h1 className={styles.title}>
                     Juicer Labs
                 </h1>
+
+                <p className={styles.description}>
+                    Check out our{" "}
+                    <span>
+                        <Link href={"https://github.com/juicer-labs"}>
+                            <a className={styles.link} href={"https://github.com/juicer-labs"}>
+                                GitHub
+                            </a>
+                        </Link>
+                    </span>!
+                </p>
             </main>
         </div>
     );
